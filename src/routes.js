57649775cb3c30211d641379ef32e3c79d31b8e4
Asad Ikhlas <asadikhlas03@gmail.com>
2019-01-27 20:32:58 +0500

    commit

---
+++ src/routes.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import {Route, Switch } from 'react-router-dom';
+
+class Routes extends Component {
+  render() {
+    return (
+        <Switch>
+            <Route path="/" exact component={Home}/>
+        </Switch>
+    )
+  }
+}
+ export default Routes;
