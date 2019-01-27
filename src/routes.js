3d83853957a0a7bf6aaa681e52251da40550915f
Asad Ikhlas <asadikhlas03@gmail.com>
2019-01-27 20:39:11 +0500

    commit

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import {Route, Switch } from 'react-router-dom';
+import Home from './component/home/home';
 
 class Routes extends Component {
   render() {
