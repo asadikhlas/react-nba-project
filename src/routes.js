530e11bcca62f4eeeee63b22f17a9fc4476b71f0
Asad Ikhlas <asadikhlas03@gmail.com>
2019-01-29 20:04:38 +0500

    comit

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,15 @@
 import React, { Component } from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./component/home/home";
-
+import Layout from './hoc/layout/layout';
 class Routes extends Component {
   render() {
     return (
-      <Switch>
-        <Route path="/" exact component={Home} />
-      </Switch>
+      <Layout>
+        <Switch>
+          <Route path="/" exact component={Home} />
+        </Switch>
+      </Layout>
     );
   }
 }
