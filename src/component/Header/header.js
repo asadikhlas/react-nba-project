11100a75bc8f7fbb70c82ebe2af5324be22bbf71
Asad Ikhlas <asadikhlas03@gmail.com>
2019-01-30 03:11:18 +0500

    comit

--- src/component/Header/header.js
+++ src/component/Header/header.js
@@ -1,40 +1,38 @@
-import React from 'react';
-import style from './header.module.css'
-import {Link} from 'react-router-dom';
-import FontAwesome from 'react-fontawesome';
-import SideNav from './sideNav/sideNav';
+import React from "react";
+import style from "./header.module.css";
+import { Link } from "react-router-dom";
+import FontAwesome from "react-fontawesome";
+import SideNav from "./sideNav/sideNav";
 
-
-const Header = (props) => {
-
-    const navBars = () => (
-        <div className={style.bars}>
-        <FontAwesome name="bars"
+const Header = props => {
+  const navBars = () => (
+    <div className={style.bars}>
+      <FontAwesome
+        name="bars"
         onClick={props.onOpenNav}
         style={{
-            color:'#dfdfdf',
-            padding:'10px',
-            cursor:'pointer'
+          color: "#dfdfdf",
+          padding: "10px",
+          cursor: "pointer"
         }}
-        />
+      />
+    </div>
+  );
 
-        </div>
-    )
-
-    const logo = () => (
-            <Link to="/" className={style.logo}>
-            <img alt="nba logo" src="/images/nba_logo.png"/>
-            </Link>
-        )
-    return (
-        <header className={style.header}>
-        <SideNav {...props} />
-        <div className={style.headerOpt}>
-            {navBars()}
-            {logo()}
-        </div>        
-        </header>
-    )
-}
+  const logo = () => (
+    <Link to="/" className={style.logo}>
+      <img alt="nba logo" src="/images/nba_logo.png" />
+    </Link>
+  );
+  return (
+    <header className={style.header}>
+      <SideNav {...props} />
+      <div className={style.headerOpt}>
+        {navBars()}
+        {logo()}
+      </div>
+    </header>
+  );
+};
 
 export default Header;
