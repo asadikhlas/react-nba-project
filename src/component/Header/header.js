245a57d14adcbbe734d1a0667d26b46a4412b481
Asad Ikhlas <asadikhlas03@gmail.com>
2019-01-29 21:41:06 +0500

    commit

--- src/component/Header/header.js
+++ src/component/Header/header.js
@@ -1,11 +1,34 @@
 import React from 'react';
-import style from './header.css'
+import style from './header.module.css'
+import {Link} from 'react-router-dom';
+import FontAwesome from 'react-fontawesome';
+
 
 const Header = () => {
+
+    const navBars = () => (
+        <div className={style.bars}>
+        <FontAwesome name="bars"
+        style={{
+            color:'#dfdfdf',
+            padding:'10px',
+            cursor:'pointer'
+        }}
+        />
+
+        </div>
+    )
+
+    const logo = () => (
+            <Link to="/" className={style.logo}>
+            <img alt="nba logo" src="/images/nba_logo.png"/>
+            </Link>
+        )
     return (
         <header className={style.header}>
         <div className={style.headerOpt}>
-            Header
+            {navBars()}
+            {logo()}
         </div>        
         </header>
     )
