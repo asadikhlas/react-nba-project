e60cfaa90e0eef320ffa634994cfeb472cde15e6
Asad Ikhlas <asadikhlas03@gmail.com>
2019-01-31 20:41:14 +0500

    commit

--- src/component/Widgets/NewsSlider/slider.js
+++ src/component/Widgets/NewsSlider/slider.js
@@ -1,15 +1,24 @@
 import React, { Component } from 'react'
+import axios from 'axios';
+import SliderTemplates from './slider_templates';
 
  class NewsSlider extends Component {
      state = {
          news: []
      }
-     componentWillMount()
-     
+     componentWillMount(){
+        axios.get(`http://localhost:3004/articles?_start=0&_end=3`)
+        .then( response =>{
+            this.setState({
+                news:response.data
+            })
+        })
+     }
+
   render() {
     return (
       <div>
-        slider
+        <SliderTemplates data={this.state.news}/>
       </div>
     )
   }
---
+++ src/component/Widgets/NewsSlider/slider_templates.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SliderTemplate = (props) => {
+    console.log(props)
+return(
+    <div>
+        slider
+    </div>
+)
+}
+
+export default SliderTemplate;
