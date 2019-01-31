822b5b2da12365d4d7894a8199ca99f0e00cc907
Asad Ikhlas <asadikhlas03@gmail.com>
2019-01-31 19:54:37 +0500

    commit

---
+++ src/component/Widgets/NewsSlider/slider.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+ class NewsSlider extends Component {
+     state = {
+         news: []
+     }
+     componentWillMount()
+     
+  render() {
+    return (
+      <div>
+        slider
+      </div>
+    )
+  }
+} 
+
+export default NewsSlider;
--- src/component/home/home.js
+++ src/component/home/home.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import NewsSlider from '../Widgets/NewsSlider/slider';
 
 const Home = () => {
     return (
         <div>
-            Home
+           <NewsSlider/>
         </div>
     )
 }
