a267f134c5fd49ca81c2f966e1136c68b87f582c
Asad Ikhlas <asadikhlas03@gmail.com>
2019-01-31 21:42:04 +0500

    commit

--- src/component/Widgets/NewsSlider/slider_templates.js
+++ src/component/Widgets/NewsSlider/slider_templates.js
@@ -1,35 +1,45 @@
-import React from 'react';
-import Slick from 'react-slick';
-const SliderTemplate = (props) => {
-    let template = null;
-   const settings = {
-       dots:true,
-       infinite:true,
-       arrows: false,
-       speed: 500,
-       slidesToShow: 1,
-       slidesToScroll: 1
-   }
+import React from "react";
+import Slick from "react-slick";
+import styles from './slider.module.css';
+import { Link } from 'react-router-dom';
+const SliderTemplate = props => {
+  let template = null;
+  const settings = {
+    dots: true,
+    infinite: true,
+    arrows: false,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  };
 
-   switch(props.type){
-       case ('featured'):
-       template = props.data.map((item, i)=>{
-           return(
-               <div key={i}>
-                    {i}
-               </div>
-           )
-       })
-       break;
-       default:
-       template = null;
-
-   }
-return(
-    <Slick {...settings}>
-        {template}
-    </Slick>
-)
-}
+  switch (props.type) {
+    case "featured":
+      template = props.data.map((item, i) => {
+        return (
+          <div key={i}>
+            <div className={styles.featured_item}>
+              <div
+                className={styles.featured_image}
+                style={{
+                  background: `url(../images/articles/${item.image})`
+                }}
+              /><div>
+                  <Link to={`/articles/${item.id}`}>
+                  <div className={styles.featured_caption}>
+                        {item.title}
+                  </div>
+                  </Link>
+              </div>
+            </div>
+          </div>
+        );
+      });
+      break;
+    default:
+      template = null;
+  }
+  return <Slick {...settings}>{template}</Slick>;
+};
 
 export default SliderTemplate;
