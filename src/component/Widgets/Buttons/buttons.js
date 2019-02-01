8683ece479206d57444c390b38cb32a853ba60a0
Asad Ikhlas <asadikhlas03@gmail.com>
2019-02-01 20:50:54 +0500

    comit

--- src/component/Widgets/Buttons/buttons.js
+++ src/component/Widgets/Buttons/buttons.js
@@ -1,32 +1,28 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import styles from './buttons.module.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import styles from "./buttons.module.css";
 
-
-const Buttons = (props) => {
-    let template = null;
-    switch(props.type){
-        case 'loadmore':
-        template = (
-            <div className={styles.blue_btn}
-            onClick={props.loadMore}
-            >
-            {props.cta}
-            </div>
-        );
-        break;
-        case 'linkTo':
-            template = (
-                    <Link to={props.linkTo}
-                    className={styles.blue_btn}>
-                        {props.cta}
-                    </Link>
-            )
-            break;
-        default:
-        template = null
-    }
-    return template;
-}
+const Buttons = props => {
+  let template = null;
+  switch (props.type) {
+    case "loadmore":
+      template = (
+        <div className={styles.blue_btn} onClick={props.loadMore}>
+          {props.cta}
+        </div>
+      );
+      break;
+    case "linkTo":
+      template = (
+        <Link to={props.linkTo} className={styles.blue_btn}>
+          {props.cta}
+        </Link>
+      );
+      break;
+    default:
+      template = null;
+  }
+  return template;
+};
 
 export default Buttons;
--- src/component/Widgets/CardInfo/cardInfo.js
+++ src/component/Widgets/CardInfo/cardInfo.js
@@ -1,27 +1,27 @@
-import React from 'react';
-import FontAwesome from 'react-fontawesome';
-import styles from './cardInfo.module.css';
+import React from "react";
+import FontAwesome from "react-fontawesome";
+import styles from "./cardInfo.module.css";
 
-const CardInfo = (props) => {
-    const teamName = (teams,team) => {
-        let data = teams.find((item)=>{
-            return item.id === team
-        });
-        if(data){
-        return data.name
-        }
+const CardInfo = props => {
+  const teamName = (teams, team) => {
+    let data = teams.find(item => {
+      return item.id === team;
+    });
+    if (data) {
+      return data.name;
     }
-    return(
-        <div className={styles.CardInfo}>
-            <span className={styles.teamName}>
-                {teamName(props.teams,props.team)}
-            </span>
-            <span className={styles.date}>
-                <FontAwesome name="clock=o" />
-                {props.date}
-            </span>
-        </div>
-    )
-}
+  };
+  return (
+    <div className={styles.CardInfo}>
+      <span className={styles.teamName}>
+        {teamName(props.teams, props.team)}
+      </span>
+      <span className={styles.date}>
+        <FontAwesome name="clock=o" />
+        {props.date}
+      </span>
+    </div>
+  );
+};
 
 export default CardInfo;
