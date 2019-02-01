59ac0e9fe113df5865dcc95a62b38e9933c99391
Asad Ikhlas <asadikhlas03@gmail.com>
2019-02-01 18:55:21 +0500

    commit

--- src/component/Widgets/NewsList/newsList.js
+++ src/component/Widgets/NewsList/newsList.js
@@ -5,9 +5,11 @@ import axios from "axios";
 import { URL } from "../../../config";
 import styles from './newsList.module.css';
 import Buttons from '../Buttons/buttons';
+import CardInfo from '../../Widgets/CardInfo/cardInfo'; 
 
 class NewsList extends Component {
   state = {
+    teams:[],
     items: [],
     start: this.props.start,
     end: this.props.start + this.props.amount,
@@ -19,6 +21,15 @@ class NewsList extends Component {
   }
 
   request = (start,end) => {
+
+    if(this.state.teams.length < 1){
+      axios.get(`${URL}/teams`)
+      .then( response => {
+        this.setState({
+          teams:response.data
+        })
+      })
+    }
     axios
     .get(`${URL}/articles?_start=${start}&_end=${end}`)
     .then(response => {
@@ -48,6 +59,7 @@ loadMore = () => {
          <div>
           <div className={styles.newslist_item}>
           <Link to={`/articles/${item.id}`}>
+          <CardInfo teams={this.state.teams} team={item.team} date={item.date}/>
             <h2>{item.title}</h2>
           </Link>
 
