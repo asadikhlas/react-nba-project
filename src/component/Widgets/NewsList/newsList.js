3b5081d533c2573d79d23285ecac6994e2b08429
Asad Ikhlas <asadikhlas03@gmail.com>
2019-02-01 17:21:43 +0500

    commit

--- src/component/Widgets/NewsList/newsList.js
+++ src/component/Widgets/NewsList/newsList.js
@@ -3,6 +3,7 @@ import { CSSTransition, TransitionGroup } from "react-transition-group";
 import { Link } from "react-router-dom";
 import axios from "axios";
 import { URL } from "../../../config";
+import styles from './newsList.module.css';
 
 class NewsList extends Component {
   state = {
@@ -13,17 +14,52 @@ class NewsList extends Component {
   };
 
   componentWillMount() {
+    this.request(this.state.start,this.state.end)
+  }
+
+  request = (start,end) => {
     axios
-      .get(`${URL}/articles?_start=${this.state.start}&_end=${this.state.end}`)
-      .then(response => {
-        this.setState({
-          items: [...this.state.items, ...response.data]
-        });
+    .get(`${URL}/articles?_start=${start}&_end=${end}`)
+    .then(response => {
+      this.setState({
+        items: [...this.state.items, ...response.data]
       });
+    });
+  }
+
+loadMore = () => {
+  let end = this.state.end + this.state.amount;
+  this.request(this.state.end,end)
+}
+  renderNews = (type) => {
+    let template = null;
+    switch(type){
+      case('card'):
+      template= this.state.items.map((item,i)=>(
+        <div>
+          <div className={styles.newslist_item}>
+          <Link to={`/articles/${item.id}`}>
+            <h2>{item.title}</h2>
+          </Link>
+
+          </div>
+        </div>
+      ))
+      break;
+      default:
+      template = null;
+    }
+    return template;
   }
   render() {
     console.log(this.state.items);
-    return <div>hello</div>;
+    return (<div>
+      { this.renderNews( this.props.type)}
+      <div onClick={()=>this.loadMore()}>
+        Load More
+      </div>
+    </div>
+    )
   }
 }
 
