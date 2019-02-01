5296cf3b2b572e60279807cc2e284d584c72bfb1
Asad Ikhlas <asadikhlas03@gmail.com>
2019-02-02 04:11:32 +0500

    comit

--- src/component/Articles/News/Post/header.js
+++ src/component/Articles/News/Post/header.js
@@ -1,17 +1,27 @@
-import React from 'react'
+import React from 'react';
+
 import TeamNfo from '../../Elements/teamNfo';
+import PostData from '../../Elements/postData';
 
- const header = (props) => {
-     const teamNfo = (team) => {
-         return team ? (
-             <TeamNfo team={team}/>
-         ):null;
+const header = (props) => {
 
-     }
-  return (
-    <div>
-      {teamNfo(props.teamData)}
-    </div>
-  )
+    const teamNfo = (team) => {
+        return team ? (
+            <TeamNfo team={team}/>
+        ):null;
+    }
+
+    const postData = ( date, author ) => (
+        <PostData data={{date,author}}/>
+    )
+    
+
+    return(
+        <div>
+            {teamNfo(props.teamData)}
+            {postData(props.date, props.author)}
+        </div>
+    )
 }
+
 export default header;
