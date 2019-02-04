168dd1938b2939830fdae8434b4a6a7049828fe3
Asad Ikhlas <asadikhlas03@gmail.com>
2019-02-04 16:00:11 +0500

    comit

--- src/component/Articles/Videos/Video/header.js
+++ src/component/Articles/Videos/Video/header.js
@@ -1,18 +1,11 @@
-import React from 'react';
-import TeamNfo from '../../Elements/teamNfo';
+import React from "react";
+import TeamNfo from "../../Elements/teamNfo";
 
-
-const header = (props) => {
-    const teamNfo = (team) => {
-        return team ? (
-            <TeamNfo team={team}/>
-        ):null;
-    }
-    return(
-        <div>
-        {teamNfo(props.teamData)}
-        </div>
-    )
-}
+const header = props => {
+  const teamNfo = team => {
+    return team ? <TeamNfo team={team} /> : null;
+  };
+  return <div>{teamNfo(props.teamData)}</div>;
+};
 
 export default header;
