1759ce3b322a0691986e4a9f027ee7e6110caf3c
Asad Ikhlas <asadikhlas03@gmail.com>
2019-02-04 15:09:02 +0500

    comit

---
+++ src/component/Articles/Videos/Video/index.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import axios from 'axios';
+import {URL} from '../../../../config';
+import styles from '../../articles.module.css';
+
+
+class VideoArticle extends Component {
+  render() {
+    return (
+      <div>
+        Videos articles 
+      </div>
+    )
+  }
+}
+
+export default VideoArticle;
--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import { Route, Switch } from "react-router-dom";
 import Home from "./component/home/home";
 import Layout from "./hoc/layout/layout";
 import NewsArticles from './component/Articles/News/Post/index';
+import VideoArticle from './component/Articles/Videos/Video/index';
 class Routes extends Component {
   render() {
     return (
@@ -10,6 +11,7 @@ class Routes extends Component {
         <Switch>
           <Route path="/" exact component={Home} />
           <Route path="/articles/:id" exact component={NewsArticles} />
+          <Route path="/videos/:id" exact component={VideoArticle}/> 
         </Switch>
       </Layout>
     );
