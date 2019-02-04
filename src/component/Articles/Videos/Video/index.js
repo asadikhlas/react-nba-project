5ba9e00bb80d90d4e4245df99a35231407fc06ae
Asad Ikhlas <asadikhlas03@gmail.com>
2019-02-04 16:00:38 +0500

    COMMIT

--- src/component/Articles/Videos/Video/index.js
+++ src/component/Articles/Videos/Video/index.js
@@ -1,71 +1,63 @@
-import React, { Component } from 'react'
-import axios from 'axios';
-import {URL} from '../../../../config';
-import styles from '../../articles.module.css';
-import Header from './header';
-import VideosRelated from '../../../Widgets/VideosList/VideosRelated/videosRelated';
-
+import React, { Component } from "react";
+import axios from "axios";
+import { URL } from "../../../../config";
+import styles from "../../articles.module.css";
+import Header from "./header";
+import VideosRelated from "../../../Widgets/VideosList/VideosRelated/videosRelated";
 
 class VideoArticle extends Component {
-    state ={
-        article: [],
-        team: [],
-        teams: [],
-        related: []
-    }
+  state = {
+    article: [],
+    team: [],
+    teams: [],
+    related: []
+  };
 
-    componentWillMount(){
-        axios
-        .get(`${URL}/videos?id=${this.props.match.params.id}`)
+  componentWillMount() {
+    axios
+      .get(`${URL}/videos?id=${this.props.match.params.id}`)
+      .then(response => {
+        let article = response.data[0];
+        axios.get(`${URL}/teams?id=${article.team}`).then(response => {
+          this.setState({
+            article,
+            team: response.data
+          });
+          this.getRelated();
+        });
+      });
+  }
+  getRelated = () => {
+    axios.get(`${URL}/teams`).then(response => {
+      let teams = response.data;
+      axios
+        .get(`${URL}/videos?q=${this.state.team[0].city}&_limit=3`)
         .then(response => {
-          let article = response.data[0];
-          axios.get(`${URL}/teams?id=${article.team}`).then(response => {
-            this.setState({
-              article,
-              team: response.data
-            });
-            this.getRelated();
+          this.setState({
+            teams,
+            related: response.data
           });
         });
-    }
-    getRelated = () => {
-        axios.get(`${URL}/teams`)
-        .then(response => {
-            let teams = response.data
-            axios.get(`${URL}/videos?q=${this.state.team[0].city}&_limit=3`)
-            .then(response => {
-                this.setState({
-                    teams,
-                    related:response.data
-                })
-
-            })
-        })
-    }
+    });
+  };
   render() {
-      const article = this.state.article;
-      const team = this.state.team;
+    const article = this.state.article;
+    const team = this.state.team;
     return (
       <div>
-          <Header teamData={team[0]}/>
+        <Header teamData={team[0]} />
         <div className={styles.videoWrapper}>
-        <h1>{article.title}</h1>
-        <iframe
-        title="videoplayer"
-        width="100%"
-        height="300px"
-        src={`https://www.youtube.com/embed/${article.url}`}
-        >
-
-        </iframe>
-
+          <h1>{article.title}</h1>
+          <iframe
+            title="videoplayer"
+            width="100%"
+            height="300px"
+            src={`https://www.youtube.com/embed/${article.url}`}
+          />
         </div>
-        <VideosRelated
-        data={this.state.related}
-        teams={this.state.teams}
-        />
+        <VideosRelated data={this.state.related} teams={this.state.teams} />
       </div>
-    )
+    );
   }
 }
 
