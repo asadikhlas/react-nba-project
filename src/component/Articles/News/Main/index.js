04866669f7829917a3937cf13782ceb042f93ce2
Asad Ikhlas <asadikhlas03@gmail.com>
2019-02-05 15:14:56 +0500

    commit

--- src/component/Articles/News/Main/index.js
+++ src/component/Articles/News/Main/index.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import NewsSlider from '../../../Widgets/NewsSlider/slider';
+import NewsList from '../../../Widgets/NewsList/newsList';
 
 const NewsMain = () => (
     <div>
 
-        NewsMain
+        <NewsSlider
+        type="featured"
+        settings={{dots:false}}
+        start={0}
+        amount={3}
+        />
+        <NewsList
+        type="card"
+        loadMore={true}
+        start={3}
+        amount={3}
+        />
     </div>
 
 )
