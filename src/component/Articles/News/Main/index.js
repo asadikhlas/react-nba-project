4ac31d43e58aec22189148578d4f5f818295ff77
Asad Ikhlas <asadikhlas03@gmail.com>
2019-02-05 15:44:57 +0500

    commit

--- src/component/Articles/News/Main/index.js
+++ src/component/Articles/News/Main/index.js
@@ -1,24 +1,17 @@
-import React from 'react';
-import NewsSlider from '../../../Widgets/NewsSlider/slider';
-import NewsList from '../../../Widgets/NewsList/newsList';
+import React from "react";
+import NewsSlider from "../../../Widgets/NewsSlider/slider";
+import NewsList from "../../../Widgets/NewsList/newsList";
 
 const NewsMain = () => (
-    <div>
-
-        <NewsSlider
-        type="featured"
-        settings={{dots:false}}
-        start={0}
-        amount={3}
-        />
-        <NewsList
-        type="cardMain"
-        loadMore={true}
-        start={3}
-        amount={3}
-        />
-    </div>
-
-)
+  <div>
+    <NewsSlider
+      type="featured"
+      settings={{ dots: false }}
+      start={0}
+      amount={3}
+    />
+    <NewsList type="cardMain" loadMore={true} start={3} amount={3} />
+  </div>
+);
 
 export default NewsMain;
