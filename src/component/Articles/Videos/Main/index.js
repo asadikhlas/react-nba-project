34118dc499901f501076936e17f4fe3ff911725e
Asad Ikhlas <asadikhlas03@gmail.com>
2019-02-05 15:45:18 +0500

    comit

--- src/component/Articles/Videos/Main/index.js
+++ src/component/Articles/Videos/Main/index.js
@@ -1,14 +1,8 @@
-import React from 'react';
-import VideosList from '../../../Widgets/VideosList/videosList';
+import React from "react";
+import VideosList from "../../../Widgets/VideosList/videosList";
 
 const VideosMain = () => (
-    <VideosList
-    type="card"
-    title={false}
-    loadmore={true}
-    start={0}
-    amount={10}
-    />
-)
+  <VideosList type="card" title={false} loadmore={true} start={0} amount={10} />
+);
 
 export default VideosMain;
