83fd2e6148865670416465cbe38c95af28e47e8a
Asad Ikhlas <asadikhlas03@gmail.com>
2019-02-05 15:42:46 +0500

    commit

--- src/routes.js
+++ src/routes.js
@@ -2,18 +2,20 @@ import React, { Component } from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./component/home/home";
 import Layout from "./hoc/layout/layout";
-import NewsArticles from './component/Articles/News/Post/index';
-import VideoArticle from './component/Articles/Videos/Video/index';
-import NewsMain from './component/Articles/News/Main/index';
+import NewsArticles from "./component/Articles/News/Post/index";
+import VideoArticle from "./component/Articles/Videos/Video/index";
+import NewsMain from "./component/Articles/News/Main/index";
+import VideoMain from './component/Articles/Videos/Main/index';
 class Routes extends Component {
   render() {
     return (
       <Layout>
         <Switch>
           <Route path="/" exact component={Home} />
-          <Route path="/news" exact component={NewsMain}/>
+          <Route path="/news" exact component={NewsMain} />
           <Route path="/articles/:id" exact component={NewsArticles} />
-          <Route path="/videos/:id" exact component={VideoArticle}/> 
+          <Route path="/videos/:id" exact component={VideoArticle} />
+          <Route path="/videos" exact component={VideoMain} />
         </Switch>
       </Layout>
     );
