67d02757646e969f6c1bd78a7b53df291545c457
Asad Ikhlas <asadikhlas03@gmail.com>
2019-02-05 15:47:12 +0500

    comit

--- src/component/Widgets/NewsList/newsList.js
+++ src/component/Widgets/NewsList/newsList.js
@@ -69,42 +69,38 @@ class NewsList extends Component {
           </CSSTransition>
         ));
         break;
-        case ('cardMain'):
+      case "cardMain":
         template = this.state.items.map((item, i) => (
           <CSSTransition
-          classNames={{
-            enter: styles.newslist_wrapper,
-            enterActive: styles.newslist_wrapper_enter
-          }}
-          timeout={500}
-          key={i}
-        >
-        <Link to={`/articles/${item.id}`}>
-        <div className={styles.flex_wrapper}>
-        <div className={styles.left}
-        style={{
-          background:`url('/images/articles/${item.image}')`
-        }}
-        >
-        <div></div>
-
-        </div>
-        <div className={styles.right}>
-        <CardInfo
+            classNames={{
+              enter: styles.newslist_wrapper,
+              enterActive: styles.newslist_wrapper_enter
+            }}
+            timeout={500}
+            key={i}
+          >
+            <Link to={`/articles/${item.id}`}>
+              <div className={styles.flex_wrapper}>
+                <div
+                  className={styles.left}
+                  style={{
+                    background: `url('/images/articles/${item.image}')`
+                  }}
+                >
+                  <div />
+                </div>
+                <div className={styles.right}>
+                  <CardInfo
                     teams={this.state.teams}
                     team={item.team}
                     date={item.date}
                   />
                   <h2>{item.title}</h2>
-
-        </div>
-
-      
-           
-          </div>
-        </Link>
-        </CSSTransition>
-        ))
+                </div>
+              </div>
+            </Link>
+          </CSSTransition>
+        ));
         break;
       default:
         template = null;
