8f92667abed5e185eec7175b33dccbfad1b45476
Asad Ikhlas <asadikhlas03@gmail.com>
2019-02-19 22:39:09 +0500

    COMMIT

--- src/component/signIn/signin.js
+++ src/component/signIn/signin.js
@@ -1,14 +1,80 @@
 import React, { Component } from 'react';
 import styles from './signin.module.css';
+import FormField from '../../component/Widgets/formFields/formFields';
 
 class SignIn extends Component {
     state = {
+        registerError:'',
+        loading:false,
+        formdata:{
+            email:{
+                element:'input',
+                value:'',
+                config:{
+                    name:'email_input',
+                    type:'email',
+                    placeholder:'Enter your email'
+                },
+                validation:{
+                    required:true,
+                    email:true
+                },
+                valid:false,
+                touched:false,
+                validationMessage:''
+            },
+            password:{
+                element:'input',
+                value:'',
+                config:{
+                    name:'password_input',
+                    type:'password',
+                    placeholder:'Enter your Password'
+                },
+                validation:{
+                    required:true,
+                    password:true
+                },
+                valid:false,
+                touched:false,
+                validationMessage:''
+            }
+        }
 
     }
+
+    updateForm = (element) => {
+        const newFormdata= {
+            ...this.state.formdata
+        }
+        const newElement = {
+            ...newFormdata[element.id]
+        }
+        newElement.value = element.event.target.value;
+        newFormdata[element.id] = newElement;
+
+        this.setState({
+            formdata:newFormdata
+        })
+    }
+
     render(){
         return(
-            <div>
-                sign in
+            <div className={styles.logContainer}>
+                <form>
+                    <h2>Register/Login</h2>
+                    <FormField
+                    id={'email'}
+                    formdata={this.state.formdata.email}
+                    change={(element)=>this.updateForm(element)}
+                    />
+                     <FormField
+                    id={'password'}
+                    formdata={this.state.formdata.password}
+                    change={(element)=>this.updateForm(element)}
+                    />
+                    
+                </form>
             </div>
         )
     }
