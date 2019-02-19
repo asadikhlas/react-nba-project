8ea4695cf7be083b1443bb455ca69b0eda688111
Asad Ikhlas <asadikhlas03@gmail.com>
2019-02-19 22:39:44 +0500

    commit

--- src/component/Widgets/formFields/formFields.js
+++ src/component/Widgets/formFields/formFields.js
@@ -1,33 +1,28 @@
-import React from 'react';
-import styles from './formFields.module.css';
+import React from "react";
+import styles from "./formFields.module.css";
 
-const FormField = ({formdata,change,id}) => {
-    const renderTemplate = () => {
-        let formTemplate = null;
-        switch(formdata.element){
-        case('input'):
+const FormField = ({ formdata, change, id }) => {
+  const renderTemplate = () => {
+    let formTemplate = null;
+    switch (formdata.element) {
+      case "input":
         formTemplate = (
-            <div>
-                <input
-                {...formdata.config}
-                value={formdata.value}
-                onBlur={(event) => change({event,id,blur:true})}
-                onChange={(event) => change({event,id,blur:false})}
-                />
-            </div>
-        )
+          <div>
+            <input
+              {...formdata.config}
+              value={formdata.value}
+              onBlur={event => change({ event, id, blur: true })}
+              onChange={event => change({ event, id, blur: false })}
+            />
+          </div>
+        );
         break;
-        default:
+      default:
         formTemplate = null;
-        }
-        return formTemplate;
-
     }
-    return(
-        <div>
-            {renderTemplate()}
-        </div>
-    )
-}
+    return formTemplate;
+  };
+  return <div>{renderTemplate()}</div>;
+};
 
 export default FormField;
