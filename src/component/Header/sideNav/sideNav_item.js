63dbc5b0a17c0cd04f47d31bcef3218b5fd66440
Asad Ikhlas <asadikhlas03@gmail.com>
2019-02-20 20:32:24 +0500

    commit

--- src/component/Header/sideNav/sideNav_item.js
+++ src/component/Header/sideNav/sideNav_item.js
@@ -1,103 +1,103 @@
 import React from "react";
 import style from "./sideNav.module.css";
-import {firebase} from '../../../firebase';
-import { Link,withRouter } from "react-router-dom";
+import { firebase } from "../../../firebase";
+import { Link, withRouter } from "react-router-dom";
 import FontAwesome from "react-fontawesome";
 
-const SideNavItem = (props) => {
-  console.log(props)
+const SideNavItem = props => {
+  console.log(props);
   const items = [
     {
       type: style.option,
       icon: "home",
-      text: "Home", 
+      text: "Home",
       link: "/",
-      login:''
+      login: ""
     },
     {
       type: style.option,
       icon: "file-text-o",
       text: "News",
       link: "/news",
-      login:''
+      login: ""
     },
     {
       type: style.option,
       icon: "play",
       text: "Videos",
       link: "/videos",
-      login:''
+      login: ""
     },
     {
       type: style.option,
       icon: "sign-in",
       text: "Dashboard",
       link: "/dashboard",
-      login:false
+      login: false
     },
     {
       type: style.option,
       icon: "sign-in",
       text: "Sign in",
       link: "/sign-in",
-      login:true
+      login: true
     },
     {
       type: style.option,
       icon: "sign-out",
       text: "Sign out",
       link: "/sign-out",
-      login:false
+      login: false
     }
   ];
 
-  const element = (item,i) => (
+  const element = (item, i) => (
     <div key={i} className={item.type}>
-          <Link to={item.link}>
-            <FontAwesome name={item.icon} />
-            {item.text}
-          </Link>
-        </div>
-  )
+      <Link to={item.link}>
+        <FontAwesome name={item.icon} />
+        {item.text}
+      </Link>
+    </div>
+  );
 
-  const restricted = (item,i) => {
+  const restricted = (item, i) => {
     let template = null;
-    if(props.user === null && item.login){
-      template = element(item,i)
+    if (props.user === null && item.login) {
+      template = element(item, i);
     }
-    if(props.user !== null && !item.login){
-        if(item.link === '/sign-out'){
-          template= (
-            <div key={i} className={item.type}
-            onClick={()=>{
-              firebase.auth().signOut()
-              .then(()=>{
-                props.history.push("/")
-              })
+    if (props.user !== null && !item.login) {
+      if (item.link === "/sign-out") {
+        template = (
+          <div
+            key={i}
+            className={item.type}
+            onClick={() => {
+              firebase
+                .auth()
+                .signOut()
+                .then(() => {
+                  props.history.push("/");
+                });
             }}
-            >
+          >
             <FontAwesome name={item.icon} />
             {item.text}
-        </div>
-          )
-        }else{
-          template = element(item,i)
-        }
+          </div>
+        );
+      } else {
+        template = element(item, i);
+      }
     }
-
 
     return template;
-  }
+  };
 
   const showItems = () => {
     return items.map((item, i) => {
-      return item.login !== '' ?
-      restricted(item,i)
-      :
-      element(item,i)
-    }); 
+      return item.login !== "" ? restricted(item, i) : element(item, i);
+    });
   };
-  
+
   return <div>{showItems()}</div>;
 };
 
