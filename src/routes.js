9c6d5ab51c8baf4d1a5c48269ffc068f97840a64
Asad Ikhlas <asadikhlas03@gmail.com>
2019-02-20 20:33:05 +0500

    commit

--- src/routes.js
+++ src/routes.js
@@ -7,19 +7,19 @@ import VideoArticle from "./component/Articles/Videos/Video/index";
 import NewsMain from "./component/Articles/News/Main/index";
 import VideoMain from "./component/Articles/Videos/Main/index";
 import SignIn from "./component/signIn/signin";
-const Routes = (props) => {
-    return (
-      <Layout user={props.user}>
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/news" exact component={NewsMain} />
-          <Route path="/articles/:id" exact component={NewsArticles} />
-          <Route path="/videos/:id" exact component={VideoArticle} />
-          <Route path="/videos" exact component={VideoMain} />
-          <Route path="/sign-in" exact component={SignIn} />
-        </Switch>
-      </Layout>
-    );
-  }
+const Routes = props => {
+  return (
+    <Layout user={props.user}>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/news" exact component={NewsMain} />
+        <Route path="/articles/:id" exact component={NewsArticles} />
+        <Route path="/videos/:id" exact component={VideoArticle} />
+        <Route path="/videos" exact component={VideoMain} />
+        <Route path="/sign-in" exact component={SignIn} />
+      </Switch>
+    </Layout>
+  );
+};
 
 export default Routes;
