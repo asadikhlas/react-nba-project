01045b71720fa1483f100f584b07c3b566bde307
Asad Ikhlas <asadikhlas03@gmail.com>
2019-02-21 17:28:13 +0500

    commit

--- src/component/Widgets/formFields/formFields.js
+++ src/component/Widgets/formFields/formFields.js
@@ -2,18 +2,15 @@ import React from "react";
 import styles from "./formFields.module.css";
 
 const FormField = ({ formdata, change, id }) => {
-
-    const showError = () => {
-        let errorMessage = null;
-        if(formdata.validation && !formdata.valid){
-            errorMessage = (
-                <div className={styles.labelError}>
-                    {formdata.validationMessage}
-                </div>
-            )
-        }
-        return errorMessage;
+  const showError = () => {
+    let errorMessage = null;
+    if (formdata.validation && !formdata.valid) {
+      errorMessage = (
+        <div className={styles.labelError}>{formdata.validationMessage}</div>
+      );
     }
+    return errorMessage;
+  };
 
   const renderTemplate = () => {
     let formTemplate = null;
@@ -31,24 +28,23 @@ const FormField = ({ formdata, change, id }) => {
           </div>
         );
         break;
-        case('select'):
+      case "select":
         formTemplate = (
           <div>
             <select
-            value={formdata.value}
-            name={formdata.config.name}
-            onBlur={event => change({ event, id, blur: true })}
-            onChange={event => change({ event, id, blur: false })}
+              value={formdata.value}
+              name={formdata.config.name}
+              onBlur={event => change({ event, id, blur: true })}
+              onChange={event => change({ event, id, blur: false })}
             >
-               {formdata.config.options.map((item,i)=>(
-                 <option key={i} value={item.id}>
-                 {item.name}
-
-                 </option>
-               ))}
+              {formdata.config.options.map((item, i) => (
+                <option key={i} value={item.id}>
+                  {item.name}
+                </option>
+              ))}
             </select>
           </div>
-        )
+        );
         break;
       default:
         formTemplate = null;
